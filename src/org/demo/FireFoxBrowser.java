1eff5da0d25cd1b1a2c8e438d092e129fc33dafc
meenakirann <meenatandra9@gmail.com>
2022-09-16 10:40:04 +0530

    first commit

---
+++ src/org/demo/FireFoxBrowser.java
@@ -0,0 +1,27 @@
+package org.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FireFoxBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver","E:\\Meena Project\\Workspace1\\SeleniumFire\\Resourse\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		//driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.flipkart.com/");
+		Thread.sleep(2000);
+		/*String title = driver.getTitle();
+	    System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		driver.close();*/
+		driver.navigate().back();
+		Thread.sleep(2000);
+		driver.navigate().forward();
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+	}
+
+}
